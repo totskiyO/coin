2558ae784dcf0ec79253c0de626fcc59f6ed5fe7
Totskiy <utkinsasha228@gmail.com>
2024-10-11 18:51:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var defaultWidth = 0;
+
+window.onload = function() {
+    var image = document.getElementById("image");
+    var counter = document.getElementById("counter");
+    var sound1000 = new Audio('1000.mp3');
+    defaultWidth = image.offsetWidth;
+
+    image.onclick = function(e) {
+        counter.textContent = parseInt(counter.textContent) + 1;
+        image.style.width = "75%";
+        if (counter.textContent == 100) {
+            sound1000.play();
+        }
+    }
+}
+
+setInterval(function() {
+    var image = document.getElementById("image");
+    image.style.width = image.offsetWidth + ((defaultWidth - image.offsetWidth) * 0.05) + "px";
+    image.style.position[1]
+}, 1/60)
---
+++ start.bat
@@ -0,0 +1,1 @@
+npx http-server H:\Projects\JavaScript\TotskiyCoin -o -p 8000
